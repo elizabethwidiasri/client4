71dd6ebc6fe0d09d12bc3f6ad1a06b8d7ac04779
elizabethwidiasri <elizabethwidiasri@gmail.com>
2019-11-15 08:47:58 +0700

    add form vue boostrap but failed, add keyboard

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -6,19 +6,47 @@
     <p class="sub-desc">n.informal</p>
     <p class="detiled-text"> Are you the fastest in conquering this set of typing race? </p>
     </div>
+
+    <!-- image -->
+    <div class="container keyboard"></div>
+    <!-- image -->
+
+    <!-- form input vue bootstrap -->
+    <div>
+    <b-form-input v-model="text" placeholder="Enter your name"></b-form-input>
+    <div class="mt-2">Ini dr vue boostrap ga keluar form nyaa uhuhuhu: {{ text }}</div>
+    </div>
+    <!-- form input vue bootstrap -->
+
+    <p>ini yg dr awal adaa</p>
     <form @submit.prevent="createRoom()">
-      <input v-model="username" type="text" placeholder="username">
-      <button type="submit">CreateRoomSSSSS</button>
+      <input v-model="username" type="text" placeholder="Enter your name">
+      <button type="submit">Play!</button>
     </form>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      text: ''
+    }
+  }
 }
 </script>
 
 <style>
+.container {
+  display: inline-block;
+  background-repeat: no-repeat;
+  background-image: url('../assets/orange.jpg');
+  background-size: center;
+  height: 500px;
+  width: 900px;
+  justify-content: flex-end;
+  align-items: flex-end;
+  margin-left: 300px;
+}
 
 </style>
