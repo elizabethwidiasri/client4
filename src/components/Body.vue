b4a036bd30feeb35b58f89a51c9bca63831561e6
dipadana <putudipadana@gmail.com>
2019-11-15 13:03:49 +0700

    udpate game and home

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,14 +1,14 @@
 <template>
-   <div class="home mt-4">
+  <div class="home mt-4">
       <!-- image -->
       <div class="container-keyboard">
-        <img src="../assets/orange.jpg" width="350" height="350" class="keyboard-img margin-right: 100px; mb-4; " >
+        <img src="../assets/orange.jpg" class="keyboard-img margin-right: 100px; mb-4; " >
       </div>
       <!-- image -->
       <!-- daring message  -->
-      
-      <p class="detiled-text mt-5" style="font-family: 'Quicksand', sans-serif; font-size: 30px; color: #444547 "> Are you the fastest in conquering this set of typing race? </p>
-      <p class="detiled-text" style="font-family: 'Quicksand', sans-serif; font-size: 15px; color: #444547 "> *psst you will be our master </p>
+
+      <p class="detiled-text1"> Are you the fastest in conquering this set of typing race? </p>
+      <p class="detiled-text2"> *psst you will be our master </p>
 
       <!-- daring message  -->
       <!-- input form  -->
@@ -22,7 +22,7 @@
       </div>
       <!-- input form  -->
   </div>
-</template> 
+</template>
 
 <script>
 export default {
@@ -43,7 +43,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 @import url('https://fonts.googleapis.com/css?family=Abril+Fatface&display=swap');
 @import url('https://fonts.googleapis.com/css?family=Quicksand&display=swap');
 .container-keyboard {
@@ -66,6 +66,46 @@ export default {
 .play-btn {
   background-color: #62B69B;
   border: none;
+}
+
+.keyboard-img{
+  width: 250px;
+  height: 250px;
+}
+
+@media screen and (min-width: 1500px) {
+  .keyboard-img{
+    width: 400px;
+    height: 400px;
+  }
+}
+
+@media screen and (min-width: 700px) {
+  .detiled-text1{
+    font-family: 'Quicksand', sans-serif;
+    font-size: 30px;
+    color: #444547;
+    margin-top: 40px;
+  }
+  .detiled-text2{
+    font-family: 'Quicksand', sans-serif;
+    font-size: 15px;
+    color: #444547
+  }
+}
+
+@media screen and (min-width: 1800px) {
+  .detiled-text1{
+  font-family: 'Quicksand', sans-serif;
+  font-size: 40px;
+  color: #444547;
+  margin-top: 110px;
+  }
+  .detiled-text2{
+  font-family: 'Quicksand', sans-serif;
+  font-size: 20px;
+  color: #444547
+}
 }
 
 </style>
